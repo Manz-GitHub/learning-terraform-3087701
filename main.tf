2fcb5299fd222a804bfe37106836613574069557
Manz-GitHub <110830366+Manz-GitHub@users.noreply.github.com>
2023-01-18 14:38:14 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ module "blog_vpc" {
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  subnet_id              = module.blog_vpc.public_subnets[0]
+  #subnet_id              = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -43,13 +43,14 @@ resource "aws_instance" "blog" {
 }
 
 module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
+  source              = "terraform-aws-modules/security-group/aws"
+  version             = "4.13.0"
 
-  vpc_id  = module.blog_vpc.vpc_id
-  name    = "blog"
-  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  vpc_id              = module.blog_vpc.vpc_id
+  subnet_id              = module.blog_vpc.public_subnets[0]
+  name                = "blog"
+  ingress_rules       = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
